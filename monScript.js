e3235b2def7c20aadd1ba150170077288b0cf6be
Sciencinfus <35138082+Sciencinfus@users.noreply.github.com>
2018-06-14 06:30:28 +0200

    Initial version

---
+++ monScript.js
@@ -0,0 +1,7 @@
+var x, y, z;  // Statement 1
+x = 10;        // Statement 2
+y = 20;        // Statement 3
+z = x + y;    // Statement 4
+
+document.getElementById("demo").innerHTML = "The value of z is " + z + ".";
+
